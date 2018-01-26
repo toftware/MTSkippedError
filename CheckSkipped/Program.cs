14a483295238a1f61c94266bc79ca3a00ed754a6
Kasper Toft Andersen <toftware@gmail.com>
2018-01-26 09:43:30 +0100

    Add MassTransit failing skipped example

---
+++ CheckSkipped/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace CheckSkipped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = CreateBus(x =>
+            {
+                x.Consumer<TestConsumer>();
+            });
+
+            bus.Start();
+            bus.Stop();
+
+            bus = CreateBus();
+
+            bus.Publish(new DummyMessage());
+
+            bus.Start();
+
+            Console.ReadKey();
+        }
+
+        static IBusControl CreateBus(Action<IReceiveEndpointConfigurator> configureEndpoint = null)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var uri = new Uri("rabbitmq://localhost/");
+
+                cfg.Host(uri, h =>
+                {
+                });
+
+                cfg.ReceiveEndpoint("Testing", e =>
+                {
+                    configureEndpoint?.Invoke(e);
+                });
+            });
+        }
+    }
+
+    class DummyMessage
+    {
+    }
+
+    class TestConsumer : IConsumer<DummyMessage>
+    {
+        public Task Consume(ConsumeContext<DummyMessage> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
